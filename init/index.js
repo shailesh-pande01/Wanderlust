6ed245faee8229cefc9f730519bf6b108b7cad6d
shailesh-pande01 <spande9013@gmail.com>
2026-07-09 15:09:55 +0530

    Redirect home to listings and update init db

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data");
 const Listing = require("../models/listing");
 
-const MONGO_URl = 'mongodb://127.0.0.1:27017/wonderlust';
+const MONGO_URl = process.env.MONGO_URL || 'mongodb+srv://shailesh_pande01:Sp35189013@cluster0.az0w0gk.mongodb.net/wonderlust?retryWrites=true&w=majority&appName=Cluster0';
 
 main().then(() => {
     console.log("connected DB");
