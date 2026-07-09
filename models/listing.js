63ecb2ac411df8fd09dbb6816516fa4bcab78cec
shailesh-pande01 <spande9013@gmail.com>
2026-07-09 14:41:39 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required:true,
+    },
+    description: String,
+    image: {
+        type: String,
+        default: "https://t3.ftcdn.net/jpg/00/29/13/38/360_F_29133877_bfA2n7cWV53fto2BomyZ6pyRujJTBwjd.jpg",
+        set: (v) => v === "" ? "https://t3.ftcdn.net/jpg/00/29/13/38/360_F_29133877_bfA2n7cWV53fto2BomyZ6pyRujJTBwjd.jpg" : v,
+    },
+    price: Number,
+    location: String,
+    country: String
+});
+
+const Listing = mongoose.model("Listing",listingSchema);
+
+module.exports = Listing;
